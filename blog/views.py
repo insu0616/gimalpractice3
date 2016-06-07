6831956642b87b50c31dd24a18c25eccc18ede4b
insoo <inu0616@gmail.com>
2016-06-08 01:38:00 +0900

    배포세팅

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post, Comment
 
 # Create your views here.
@@ -6,3 +6,22 @@ from .models import Post, Comment
 def index(request):
     post_list = Post.objects.all()
     return render(request, 'blog/index.html', {'post_list':post_list})
+
+def detail(request, pk):
+    post_detail = get_object_or_404(Post, pk=pk)
+    post_detail.view += 1
+    post_detail.save()
+    return render(request, 'blog/detail.html', {'post_detail':post_detail})
+
+def post_new(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            messages.success(request, '글이 생성됐습니다.')
+            return redirect(reverse('blog:detail', args=[post.pk]))
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_form.html', {'form': form})
